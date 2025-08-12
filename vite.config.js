4a8acf253bbadaa28c3779dd81abbc70679db291
Zachary Baca <zachary.baca@msn.com>
2025-08-12 07:10:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
+  server: {
+    port: 5173,
+    fs: { strict: false },
+    mimeTypes: {
+      webmanifest: 'application/manifest+json',
+    },
+    historyApiFallback: {
+      disableDotRule: true, // Ensures non-HTML files aren't caught in SPA routing
+    },
+  build: {
     outDir: 'dist', // ✅ Ensures dist exists after build
     assetsDir: 'assets', // ✅ Used for bundled assets
   }
