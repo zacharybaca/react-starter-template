08329a5353b0ddc50e62ba0d3d876a240cd3b5f7
Zachary Baca <zachary.baca@msn.com>
2025-08-12 07:05:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    outDir: 'dist', // ✅ Ensures dist exists after build
+    assetsDir: 'assets', // ✅ Used for bundled assets
+  }
 })
