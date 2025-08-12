ae426403f4694d421bceb751ea8c06e187a0b65d
Zachary Baca <zachary.baca@msn.com>
2025-08-12 07:41:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'modern-normalize'
 import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
