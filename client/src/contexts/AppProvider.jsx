cd24174c07d5a64b0d350b858f84d3ffe36932ef
Zachary Baca <zachary.baca@msn.com>
2026-01-07 14:21:42 -0600

    Update AppProvider.jsx

--- client/src/contexts/AppProvider.jsx
+++ client/src/contexts/AppProvider.jsx
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { FetcherProvider } from './Fetcher/FetcherProvider';
 
 
 export const AppProvider = ({ children }) => {
   return (
     <Router>
-    
+      <FetcherProvider>
+        {children}
+      </FetcherProvider>
     </Router>
   )
 }
