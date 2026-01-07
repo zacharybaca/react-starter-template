7d18c1284773e743dda31c4c0261ae6f63cb6128
Zachary Baca <zachary.baca@msn.com>
2026-01-07 08:56:58 -0600

    Implement user authentication controller methods

---
+++ server/controllers/authController.js
@@ -0,0 +1,73 @@
+
+import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js"; // We'll fill this utility next
+import asyncHandler from "express-async-handler";
+
+// @desc    Register new user
+// @route   POST /api/auth/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id); // Generate JWT and set cookie
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc    Auth user & get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Check if user exists AND password matches (method defined in Model)
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id); // Generate JWT and set cookie
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc    Logout user
+// @route   POST /api/auth/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+  // Clear the cookie by setting it to expire immediately
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
+});
+
+export { registerUser, loginUser, logoutUser };
