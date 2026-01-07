199e751c4cf1b79f2d8a280d2d84f7792bfbb20e
Zachary Baca <zachary.baca@msn.com>
2026-01-07 10:02:25 -0600

    Refactor NavBar to use NavLink for navigation

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,37 @@
-import './nav-bar.css'
+import { Link, NavLink } from "react-router-dom";
+import "./nav-bar.css";
 
 const NavBar = () => {
-
   return (
-    <nav>
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/register"
+            className={({ isActive }) => (isActive ? "active-link" : "")}
+          >
+            Register
+          </NavLink>
+        </li>
+      </ul>
     </nav>
-  )
-
-}
+  );
+};
 
 export default NavBar;
