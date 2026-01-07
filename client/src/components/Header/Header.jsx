443bb15a63b1956675b9104cdd59b72e085096f9
Zachary Baca <zachary.baca@msn.com>
2026-01-07 10:03:56 -0600

    Refactor Header component to include NavBar and logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
-import './header.css'
+import { Link } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import "./header.css";
 
 const Header = () => {
-
-    return (
-        <header>
-
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <Link to="/" className="logo-link">
+          {/* Replace this text with an <img> tag for a real logo later */}
+          <h1>MERN Starter</h1>
+        </Link>
+      </div>
+      <NavBar />
+    </header>
+  );
+};
 
 export default Header;
