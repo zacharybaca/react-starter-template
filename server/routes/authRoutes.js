471e6ee4a4c12b15ab2f1b5566bc0a04cb1c5f85
Zachary Baca <zachary.baca@msn.com>
2026-03-31 08:17:06 -0500

    Add new authentication routes for admin and password reset

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,11 @@
-
 import express from "express";
 import {
   loginUser,
   registerUser,
   logoutUser,
+  isUserAdmin,
+  forgotPassword,
+  resetPassword,
 } from "../controllers/authController.js";
 
 const router = express.Router();
@@ -11,5 +13,8 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/is-admin", isUserAdmin); // Route to check if the user is an admin
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 export default router;
