7dd2e79d61d02b274ccdae2179288a45a531c265
Zachary Baca <zachary.baca@msn.com>
2026-03-31 08:54:01 -0500

    Create Home.jsx

---
+++ client/src/components/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+
+      
+      <div className="all-companies-section">
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
