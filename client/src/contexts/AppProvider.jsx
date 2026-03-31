c631aef4d299a88cce6190d57bddce9f2820616a
Zachary Baca <zachary.baca@msn.com>
2026-03-31 07:15:39 -0500

    Remove Router and wrap children with FetcherProvider

--- client/src/contexts/AppProvider.jsx
+++ client/src/contexts/AppProvider.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { FetcherProvider } from './Fetcher/FetcherProvider';
 
 
 export const AppProvider = ({ children }) => {
   return (
-    <Router>
+    <FetcherProvider>
       {children}
-    </Router>
+    </FetcherProvider>
   )
 }
