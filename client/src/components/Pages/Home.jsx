97f0e161c65b468f591573baaf50649cc90f386a
Zachary Baca <zachary.baca@msn.com>
2026-05-26 12:38:32 -0500

    Refactor Home component layout
    Removed unnecessary div and adjusted structure.

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
   return (
     <div className="home-page">
+      <section className="hero-section">
+        <div className="container">
+          <h1>
+            <span className="hero-accent">Name of App Goes Here</span>
+          </h1>
+          <p>
+            Explanation of App or App Slogan Goes Here
+          </p>
+        </div>
+      </section>
 
-      
-      <div className="all-companies-section">
-
+      <div className="page-content">
+        <section className="all-companies-section">
+          <h2>This Component Wraps Around All Your Other Components</h2>
+        </section>
       </div>
     </div>
   );
