4f6443f2ec8b6516d42c4e83535b982ef36326ae
Zachary Baca <zachary.baca@msn.com>
2026-05-26 12:31:22 -0500

    Wrap children with SocketProvider in AppProvider

--- client/src/contexts/AppProvider.jsx
+++ client/src/contexts/AppProvider.jsx
@@ -1,12 +1,14 @@
 import { FetcherProvider } from './Fetcher/FetcherProvider';
 import { AuthProvider } from './Auth/AuthProvider';
-
+import { SocketProvider } from './Socket/SocketProvider';
 
 export const AppProvider = ({ children }) => {
   return (
     <FetcherProvider>
       <AuthProvider>
-        {children}
+        <SocketProvider>
+          {children}
+        </SocketProvider>
       </AuthProvider>
     </FetcherProvider>
   )
