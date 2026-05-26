f7a73f7d9bceb5a1dae4f07039d618e6e93c599e
Zachary Baca <zachary.baca@msn.com>
2026-05-26 12:58:44 -0500

    Update cookie settings for production environment

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: process.env.NODE_ENV !== "development" ? "none" : "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
